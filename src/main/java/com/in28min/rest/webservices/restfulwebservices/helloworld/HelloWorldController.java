5535cc2fee39ed116e4d90a876d9258604217f19
JHTran <tr.h.jonathan@gmail.com>
2024-01-28 15:08:00 +0900

    last commit

--- src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.in28min.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 // REST API
 @RestController
 public class HelloWorldController {
+    
+    private MessageSource messageSource;
+    
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 	
 	// "/hello-world"
     @GetMapping("/hello-world")
@@ -25,6 +35,14 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        
+        Locale locale = LocaleContextHolder.getLocale();
+        
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
     
 }
